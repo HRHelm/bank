54439a5cda756f6981ce4d5290817d106abb3bf0
HRHelm <hendrikroland@gmail.com>
2022-01-27 16:18:45 +0200

    TASK-4 added method deleteAccount

--- src/main/java/ee/bcs/bank/restbank/AccountService.java
+++ src/main/java/ee/bcs/bank/restbank/AccountService.java
@@ -93,5 +93,21 @@ public class AccountService {
     }
 
 
+    public RequestResult deleteAccount(List<AccountDto> accounts, int accountId) {
+        RequestResult requestResult = new RequestResult();
 
+        if (!accountIdExist(accounts, accountId)) {
+            requestResult.setError("Account ID: " + accountId + "does not exist!");
+            requestResult.setAccountId(accountId);
+            return requestResult;
+        }
+
+        AccountDto account = getAccountByID(accounts, accountId);
+        accounts.remove(account);
+
+        requestResult.setMessage("Account deleted");
+        requestResult.setAccountId(accountId);
+
+        return requestResult;
+    }
 }
